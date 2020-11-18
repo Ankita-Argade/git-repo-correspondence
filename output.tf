8fbd52c0cfc351003a6cc3741092689585a12418
Ankita Argade <71265200+Ankita-Argade@users.noreply.github.com>
2020-11-18 14:53:46 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,7 +0,0 @@
-output "approval" {
-  value = "${path.module}/modules/ecm_correspondence/email_templates/Vertex_Approval_Template.html"
-}
-
-output "rejection" {
-  value = "${path.module}/modules/ecm_correspondence/email_templates/Vertex_Rejection_Template.html"
-}
