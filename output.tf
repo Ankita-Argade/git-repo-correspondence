a600c1c991e8b954103297f1435edea582bc64ca
Ankita Argade <71265200+Ankita-Argade@users.noreply.github.com>
2020-11-18 18:27:34 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "approval" {
+  value = "${path.module}/modules/ecm_correspondence/email_templates/Vertex_Approval_Template.html"
+}
+
+output "rejection" {
+  value = "${path.module}/modules/ecm_correspondence/email_templates/Vertex_Rejection_Template.html"
+}
