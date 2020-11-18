d20353297bd40904909217991b21d6cc3e8520d0
Ankita Argade <71265200+Ankita-Argade@users.noreply.github.com>
2020-11-18 17:17:34 +0530

    Update output.tf

--- modules/ecm_correspondence/output.tf
+++ modules/ecm_correspondence/output.tf
@@ -1,7 +1,7 @@
 output "approval" {
-  value = "${path.module}/modules/ecm_correspondence/email_templates/Vertex_Approval_Template.html"
+  value = "${path.module}/email_templates/Vertex_Approval_Template.html"
 }
 
 output "rejection" {
-  value = "${path.module}/modules/ecm_correspondence/email_templates/Vertex_Rejection_Template.html"
+  value = "${path.module}/email_templates/Vertex_Rejection_Template.html"
 }
